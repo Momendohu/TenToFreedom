e8ca20036d96b49da5280ee1cdf1afc0c96cab87
阿保達也 <pinog.jh@gmail.com>
2018-11-28 23:15:27 +0900

    微調整

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,8 +24,8 @@ public class Enemy : MonoBehaviour {
     }
 
     private State state = new State {
-        Hp = 1,
-        MaxHp = 1,
+        Hp = 10,
+        MaxHp = 10,
     };
 
     //=============================================================
