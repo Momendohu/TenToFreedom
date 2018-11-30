6729cb52562384eb4bcb4de2273ac1b5c0cf0dfa
阿保達也 <pinog.jh@gmail.com>
2018-11-30 14:30:31 +0900

    yupoint定義

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 //using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-    
+    /// <summary>
+    /// パラメーター(大域的な)
+    /// </summary>
+    public struct Parameter {
+        public float YuPoint;
+    }
+
+    public Parameter paramater = new Parameter {
+        YuPoint = 0,
+    };
 }
